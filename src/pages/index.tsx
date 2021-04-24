eef9612619aae46c6151f0b53cfcc80a28d20ac3
Frederico Velho <fredericovcoelho@gmail.com>
2021-04-24 00:02:14 -0300

    fetch data

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+
+export default function Home(props) {
+  return (
+    <>
+      <h1>index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+  console.log(data)
+  return {
+    props:{
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
